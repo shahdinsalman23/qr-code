42a9d27bb032da72f4354223ddb4da4833766191
Ahtisham hussain <147423967+ahteshamniqosa@users.noreply.github.com>
2024-08-13 04:24:55 -0700

    //ahtisham

--- src/home/landing.js
+++ src/home/landing.js
@@ -1,14 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import { baseurl } from '../baseurl';
-import Header from '../header/header';
-import Hero from '../components/Hero';
 import Generatetypesqrcode from '../components/Generatetypesqrcode';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from '../Footer/Footer';
-
-
 import { baseurl } from '../baseurl';
 import Header from '../header/header';
 import Hero from '../components/Hero';
@@ -54,41 +49,6 @@ const Landing = () => {
 
 
 
-  let data = '';
-
-  const [tracking_number, settracking_number] = useState('')
-  const demo = async () => {
-
-    const formData = {
-      tracking_number: tracking_number,
-
-    }
-    try {
-
-
-      // Make a POST request using axios
-      const response = await axios.post(`${baseurl}/trackpackage`, formData);
-      console.log(tracking_number, "track")
-      console.log(response.data, "track")
-
-      data = response.data;
-
-      setr(response.data.ShipmentData)
-
-      setshow(true)
-
-
-
-
-      // Set the response data in state
-
-    } catch (error) {
-      // Handle errors
-      console.error('Error:', error.message);
-      // Display an error message to the user
-      alert('An error occurred. Please try again later.');
-    }
-  };
 
 
 
@@ -98,10 +58,10 @@ const Landing = () => {
       <Header />
       <Hero />
       <Generator />
+      <Generatetypesqrcode/>
       <Marketing />
       <Everything />
       <Leave /> 
-      <Generatetypesqrcode/>
       <Days/>
       <Footer/>
     </>
