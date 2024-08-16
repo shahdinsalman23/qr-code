d640cace8de407b3c5ea4cc3b50ed67e9a86593d
Shahdin Salman <154451265+MTalha24@users.noreply.github.com>
2024-08-15 18:53:43 -0600

    //

--- src/components/Appearance.js
+++ src/components/Appearance.js
@@ -1,12 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
-import { baseurl } from '../baseurl';
-// import step1 from '../img/steps1.svg';
-// import step2 from '../img/steps2.svg';
-// import step3 from '../img/steps.svg';
 
 import Qr1 from '../img/Qr1.png';
 import Qr2 from '../img/Qr2.png';
@@ -20,18 +14,7 @@ import Qr9 from '../img/Qr9.png';
 import Qr10 from '../img/Qr10.png';
 import Qr11 from '../img/Qr11.png';
 
-
 const Appearance = () => {
-
-
-
-
-
-
-
-
-
-
     return (
         <>
             <div className="tab-2-hero">
@@ -50,103 +33,191 @@ const Appearance = () => {
                         </div>
 
                         <div className="qr-tab-box">
+                            {/* Frame Tab */}
                             <TabPanel>
                                 <div className="ar-bar-box">
+                                    <Tabs>
+                                        <TabList>
+                                            <Tab>Option 1</Tab>
+                                            <Tab>Option 2</Tab>
+                                            <Tab>Option 3</Tab>
+                                        </TabList>
 
-                                    <div className=" qr-card none-qr-card">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
-                                            <path d="M12 0.0200195C18.6278 0.0200195 24 5.39227 24 12.02C24 18.6478 18.6278 24.02 12 24.02C5.37225 24.02 0 18.6478 0 12.02C0 5.39227 5.37225 0.0200195 12 0.0200195ZM1.3335 12.02C1.3335 17.9113 6.10875 22.6865 12 22.6865C14.7022 22.6865 17.1705 21.6815 19.05 20.0248L4.03725 4.92277C2.35575 6.80827 1.3335 9.29452 1.3335 12.02ZM12 1.35352C9.3135 1.35352 6.8595 2.34652 4.98375 3.98527L19.9928 19.0835C21.6562 17.2025 22.6665 14.729 22.6665 12.02C22.6665 6.12877 17.8913 1.35352 12 1.35352Z" fill="black" />
-                                        </svg>
-                                    </div>
+                                        <TabPanel>
+                                            <div className="qr-card">
+                                                <img src={Qr1} alt="" />
+                                            </div>
+                                            <div className="qr-card">
+                                                <img src={Qr2} alt="" />
+                                            </div>
+                                            <div className="qr-card">
+                                                <img src={Qr3} alt="" />
+                                            </div>
+                                        </TabPanel>
+                                        <TabPanel>
+                                            <div className="qr-card">
+                                                <img src={Qr4} alt="" />
+                                            </div>
+                                            <div className="qr-card">
+                                                <img src={Qr5} alt="" />
+                                            </div>
+                                            <div className="qr-card">
+                                                <img src={Qr6} alt="" />
+                                            </div>
+                                        </TabPanel>
+                                        <TabPanel>
+                                            <div className="qr-card">
+                                                <img src={Qr7} alt="" />
+                                            </div>
+                                            <div className="qr-card">
+                                                <img src={Qr8} alt="" />
+                                            </div>
+                                            <div className="qr-card">
+                                                <img src={Qr9} alt="" />
+                                            </div>
+                                        </TabPanel>
+                                    </Tabs>
+                                </div>
+                            </TabPanel>
 
-                                    <div className="qr-card">
-                                        <img src={Qr1} alt="" />
-                                    </div>
+                            {/* Shape Tab */}
+                            <TabPanel>
+                                <div className="ar-bar-box">
+                                    <Tabs>
+                                        <TabList>
+                                            <Tab>Option 1</Tab>
+                                            <Tab>Option 2</Tab>
+                                            <Tab>Option 3</Tab>
+                                        </TabList>
 
-                                    <div className="qr-card">
-                                        <img src={Qr2} alt="" />
-                                    </div>
+                                        <TabPanel>
+                                            <div className="qr-card">
+                                                <img src={Qr10} alt="" />
+                                            </div>
+                                            <div className="qr-card">
+                                                <img src={Qr11} alt="" />
+                                            </div>
+                                            <div className="qr-card">
+                                                <img src={Qr1} alt="" />
+                                            </div>
+                                        </TabPanel>
+                                        <TabPanel>
+                                            <div className="qr-card">
+                                                <img src={Qr2} alt="" />
+                                            </div>
+                                            <div className="qr-card">
+                                                <img src={Qr3} alt="" />
+                                            </div>
+                                            <div className="qr-card">
+                                                <img src={Qr4} alt="" />
+                                            </div>
+                                        </TabPanel>
+                                        <TabPanel>
+                                            <div className="qr-card">
+                                                <img src={Qr5} alt="" />
+                                            </div>
+                                            <div className="qr-card">
+                                                <img src={Qr6} alt="" />
+                                            </div>
+                                            <div className="qr-card">
+                                                <img src={Qr7} alt="" />
+                                            </div>
+                                        </TabPanel>
+                                    </Tabs>
+                                </div>
+                            </TabPanel>
 
-                                    <div className="qr-card">
-                                        <img src={Qr3} alt="" />
-                                    </div>
+                            {/* Logo Tab */}
+                            <TabPanel>
+                                <div className="ar-bar-box">
+                                    <Tabs>
+                                        <TabList>
+                                            <Tab>Option 1</Tab>
+                                            <Tab>Option 2</Tab>
+                                            <Tab>Option 3</Tab>
+                                        </TabList>
 
-                                    <div className="qr-card">
-                                        <img src={Qr4} alt="" />
-                                    </div>
+                                        <TabPanel>
+                                            <div className="qr-card">
+                                                <img src={Qr8} alt="" />
+                                            </div>
+                                            <div className="qr-card">
+                                                <img src={Qr9} alt="" />
+                                            </div>
+                                            <div className="qr-card">
+                                                <img src={Qr10} alt="" />
+                                            </div>
+                                        </TabPanel>
+                                        <TabPanel>
+                                            <div className="qr-card">
+                                                <img src={Qr11} alt="" />
+                                            </div>
+                                            <div className="qr-card">
+                                                <img src={Qr1} alt="" />
+                                            </div>
+                                            <div className="qr-card">
+                                                <img src={Qr2} alt="" />
+                                            </div>
+                                        </TabPanel>
+                                        <TabPanel>
+                                            <div className="qr-card">
+                                                <img src={Qr3} alt="" />
+                                            </div>
+                                            <div className="qr-card">
+                                                <img src={Qr4} alt="" />
+                                            </div>
+                                            <div className="qr-card">
+                                                <img src={Qr5} alt="" />
+                                            </div>
+                                        </TabPanel>
+                                    </Tabs>
+                                </div>
+                            </TabPanel>
 
-                                    <div className="qr-card">
-                                        <img src={Qr5} alt="" />
-                                    </div>
+                            {/* Level Tab */}
+                            <TabPanel>
+                                <div className="ar-bar-box">
+                                    <Tabs>
+                                        <TabList>
+                                            <Tab>Option 1</Tab>
+                                            <Tab>Option 2</Tab>
+                                            <Tab>Option 3</Tab>
+                                        </TabList>
 
-                                    <div className="qr-card">
-                                        <img src={Qr6} alt="" />
-                                    </div>
-
-                                    <div className="qr-card">
-                                        <img src={Qr7} alt="" />
-                                    </div>
-
-                                    <div className="qr-card">
-                                        <img src={Qr8} alt="" />
-                                    </div>
-
-                                    <div className="qr-card">
-                                        <img src={Qr9} alt="" />
-                                    </div>
-
-                                    <div className="qr-card">
-                                        <img src={Qr10} alt="" />
-                                    </div>
-
-                                    <div className="qr-card">
-                                        <img src={Qr11} alt="" />
-                                    </div>
-
-
-                                    <div className="qr-card">
-                                        <img src={Qr1} alt="" />
-                                    </div>
-
-                                    <div className="qr-card">
-                                        <img src={Qr2} alt="" />
-                                    </div>
-
-                                    <div className="qr-card">
-                                        <img src={Qr3} alt="" />
-                                    </div>
-
-                                    <div className="qr-card">
-                                        <img src={Qr4} alt="" />
-                                    </div>
-
-                                    <div className="qr-card">
-                                        <img src={Qr5} alt="" />
-                                    </div>
-
-                                    <div className="qr-card">
-                                        <img src={Qr6} alt="" />
-                                    </div>
-
-                                    <div className="qr-card">
-                                        <img src={Qr7} alt="" />
-                                    </div>
-
-                                    <div className="qr-card">
-                                        <img src={Qr8} alt="" />
-                                    </div>
-
-                                    <div className="qr-card">
-                                        <img src={Qr9} alt="" />
-                                    </div>
-
-                                    <div className="qr-card">
-                                        <img src={Qr10} alt="" />
-                                    </div>
-
-                                    <div className="qr-card">
-                                        <img src={Qr11} alt="" />
-                                    </div>
+                                        <TabPanel>
+                                            <div className="qr-card">
+                                                <img src={Qr6} alt="" />
+                                            </div>
+                                            <div className="qr-card">
+                                                <img src={Qr7} alt="" />
+                                            </div>
+                                            <div className="qr-card">
+                                                <img src={Qr8} alt="" />
+                                            </div>
+                                        </TabPanel>
+                                        <TabPanel>
+                                            <div className="qr-card">
+                                                <img src={Qr9} alt="" />
+                                            </div>
+                                            <div className="qr-card">
+                                                <img src={Qr10} alt="" />
+                                            </div>
+                                            <div className="qr-card">
+                                                <img src={Qr11} alt="" />
+                                            </div>
+                                        </TabPanel>
+                                        <TabPanel>
+                                            <div className="qr-card">
+                                                <img src={Qr1} alt="" />
+                                            </div>
+                                            <div className="qr-card">
+                                                <img src={Qr2} alt="" />
+                                            </div>
+                                            <div className="qr-card">
+                                                <img src={Qr3} alt="" />
+                                            </div>
+                                        </TabPanel>
+                                    </Tabs>
                                 </div>
                             </TabPanel>
                         </div>
@@ -155,7 +226,8 @@ const Appearance = () => {
             </div>
         </>
     )
-
 }
 
-export default Appearance
+export default Appearance;
+
+
